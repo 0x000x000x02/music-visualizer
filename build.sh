7d6560c759fc98c76283162910e4d8f60ef3f86e
0x000x000x02 <abinr5501@gmail.com>
2021-06-02 20:04:00 +0530

    Added aquila build for linux

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 mkdir build
-g++ -o "build/main" "GLFW Stuff/main.cpp" "GLFW Stuff/glad.c" -lsfml-audio -lsfml-system -lglfw3 -lGL -lX11 -lpthread -lXrandr -lXi -ldl -lAquila -I"GLFW Stuff/"
-mkdir "build/shaders"
+g++ -o "build/main" "GLFW Stuff/main.cpp" "GLFW Stuff/glad.c" -I"./extras/linux/aquila/include" -L"./extras/linux/aquila/lib" -lsfml-audio -lsfml-system -lglfw -lGL -lX11 -lpthread -lXrandr -lXi -ldl -lAquila -I"GLFW Stuff/"
+mkdir "build/shaders" && \
 cp -r "GLFW Stuff/shaders/" "build/"
